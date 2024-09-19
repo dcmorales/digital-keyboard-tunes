36078fdb92e297a92e677649153f69940f52234a
DC Morales <dc.morales37@gmail.com>
2024-09-18 23:40:07 -0700

    fix intentional linting error

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link';
-
 export default function Home() {
 	return (
 		<div>
 			<h1>Digital Keyboard Tunes</h1>
-			<p>New version coming soon...</p>
+			<p>New version coming soon.....</p>
 		</div>
 	);
 }
