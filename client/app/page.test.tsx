99fa4a235efe5c7fb739a8769ec6311649dd1247
DC Morales <dc.morales37@gmail.com>
2024-09-19 18:20:54 -0700

    update test suite

--- client/app/page.test.tsx
+++ client/app/page.test.tsx
@@ -1,10 +1,12 @@
-import { expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Page from './page';
 
-test('Page', () => {
-	render(<Page />);
-	expect(
-		screen.getByRole('heading', { level: 1, name: 'Digital Keyboard Tunes' })
-	).toBeDefined();
+describe('Page', () => {
+	it('should render a heading', () => {
+		render(<Page />);
+		expect(
+			screen.getByRole('heading', { level: 1, name: 'Digital Keyboard Tunes' })
+		).toBeDefined();
+	});
 });
