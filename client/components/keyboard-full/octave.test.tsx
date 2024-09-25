8bc3b04b9f2ed17670b62859b3c65e57c50dea3a
DC Morales <dc.morales37@gmail.com>
2024-09-24 18:01:55 -0700

    add keys to octave

--- client/components/keyboard-full/octave.test.tsx
+++ client/components/keyboard-full/octave.test.tsx
@@ -1,15 +1,27 @@
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
 import Octave from './octave';
 
 describe('Octave', () => {
-	it('renders a labeled group div', () => {
+	beforeEach(() => {
 		render(<Octave octNum={1} />);
+	});
 
+	afterEach(cleanup);
+
+	it('renders a labeled group div', () => {
 		expect(
 			screen.getByRole('group', {
 				name: 'Octave #1',
 			})
 		).toBeDefined();
+	});
+
+	it('renders 12 keys', () => {
+		expect(
+			screen.getByRole('group', {
+				name: 'Octave #1',
+			}).children.length
+		).toBe(12);
 	});
 });
--- client/components/keyboard-full/octave.tsx
+++ client/components/keyboard-full/octave.tsx
@@ -1,11 +1,30 @@
+import Key from './key';
+
 interface OctaveProps {
 	octNum: number;
 }
 
+const noteOptions = [
+	'C',
+	'D&#9837;',
+	'D',
+	'E&#9837;',
+	'E',
+	'F',
+	'G&#9837;',
+	'G',
+	'A&#9837;',
+	'A',
+	'B&#9837;',
+	'B',
+];
+
 export default function Octave({ octNum }: OctaveProps): JSX.Element {
 	return (
 		<div role="group" aria-label={`Octave #${octNum}`}>
-			...
+			{noteOptions.map((noteOption) => {
+				return <Key key={noteOption} note={`${noteOption}${octNum}`} />;
+			})}
 		</div>
 	);
 }
