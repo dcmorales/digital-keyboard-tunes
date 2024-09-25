7bd6597f09457da8c49be76ccf35caef53151ae9
DC Morales <dc.morales37@gmail.com>
2024-09-24 18:12:04 -0700

    add octaves to full keyboard

--- client/components/keyboard-full/keyboard-full.test.tsx
+++ client/components/keyboard-full/keyboard-full.test.tsx
@@ -1,15 +1,27 @@
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
 import KeyboardFull from './keyboard-full';
 
 describe('Full Keyboard', () => {
-	it('renders a labeled group div', () => {
+	beforeEach(() => {
 		render(<KeyboardFull />);
+	});
 
+	afterEach(cleanup);
+
+	it('renders a labeled group div', () => {
 		expect(
 			screen.getByRole('group', {
 				name: 'Full Keyboard Keys',
 			})
 		).toBeDefined();
+	});
+
+	it('renders 8 octaves', () => {
+		expect(
+			screen.getByRole('group', {
+				name: 'Full Keyboard Keys',
+			}).children.length
+		).toBe(8);
 	});
 });
--- client/components/keyboard-full/keyboard-full.tsx
+++ client/components/keyboard-full/keyboard-full.tsx
@@ -1,7 +1,14 @@
+import Octave from './octave';
+
+// create an array of numbers 1-8
+const octaveNums = Array.from({ length: 8 }, (_, index) => index + 1);
+
 export default function KeyboardFull(): JSX.Element {
 	return (
 		<div role="group" aria-label="Full Keyboard Keys">
-			...
+			{octaveNums.map((octaveNum) => {
+				return <Octave key={octaveNum} octNum={octaveNum} />;
+			})}
 		</div>
 	);
 }
