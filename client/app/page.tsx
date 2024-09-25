0f45d74c443f89e7330493ab47ae9075897d4581
DC Morales <dc.morales37@gmail.com>
2024-09-24 18:16:51 -0700

    add keyboard to page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,11 @@
+import KeyboardFull from '@/components/keyboard-full/keyboard-full';
+
 export default function Home(): JSX.Element {
 	return (
 		<div>
 			<h1>Digital Keyboard Tunes</h1>
+
+			<KeyboardFull />
 		</div>
 	);
 }
