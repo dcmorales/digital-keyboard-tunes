3496643a8d14fbfb21a6d1b329a48f516ff07c5d
DC Morales <dc.morales37@gmail.com>
2024-09-27 12:37:51 -0700

    create scrollbar

---
+++ client/components/common/scrollbar.tsx
@@ -0,0 +1,18 @@
+interface ScrollbarProps {
+	children: React.ReactNode;
+}
+
+export default function Scrollbar({ children }: ScrollbarProps): JSX.Element {
+	return (
+		<div
+			className="scrollbar"
+			role="scrollbar"
+			aria-controls="scrollable-content"
+			aria-valuenow={0}
+			aria-label="Scrollable area"
+			tabIndex={0}
+		>
+			<div id="scrollable-content">{children}</div>
+		</div>
+	);
+}
