aa39bb18402af6eaa54ef2792a022523ec34fb06
DC Morales <dc.morales37@gmail.com>
2024-09-28 16:31:35 -0700

    add click action to button

--- client/components/common/custom-button.test.tsx
+++ client/components/common/custom-button.test.tsx
@@ -1,11 +1,14 @@
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import CustomButton from './custom-button';
 
 describe('Header', () => {
+	const handleClick = vi.fn();
 	const { getByText } = render(
-		<CustomButton ariaLabel="Test the Button">Test Button</CustomButton>
+		<CustomButton ariaLabel="Test the Button" onClick={handleClick}>
+			Test Button
+		</CustomButton>
 	);
 
 	it('renders the button with children', () => {
@@ -13,5 +16,13 @@ describe('Header', () => {
 			screen.getByRole('button', { name: 'Test the Button' })
 		).toBeDefined();
 		expect(getByText('Test Button')).toBeDefined();
+	});
+
+	it('calls onClick when clicked', () => {
+		const button = screen.getByRole('button', { name: 'Test the Button' });
+
+		fireEvent.click(button);
+
+		expect(handleClick).toHaveBeenCalledTimes(1);
 	});
 });
--- client/components/common/custom-button.tsx
+++ client/components/common/custom-button.tsx
@@ -3,14 +3,16 @@
 interface CustomButtonProps {
 	children: React.ReactNode;
 	ariaLabel: string;
+	onClick: () => void;
 }
 
 export default function CustomButton({
 	children,
 	ariaLabel,
+	onClick,
 }: CustomButtonProps): JSX.Element {
 	return (
-		<button className="custom-button" aria-label={ariaLabel}>
+		<button className="custom-button" aria-label={ariaLabel} onClick={onClick}>
 			{children}
 		</button>
 	);
