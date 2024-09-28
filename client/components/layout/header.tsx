24796470fd68791ddbb7712ea37e378b04d6ed37
DC Morales <dc.morales37@gmail.com>
2024-09-28 16:12:15 -0700

    adjust header layout

--- client/components/layout/header.tsx
+++ client/components/layout/header.tsx
@@ -7,11 +7,13 @@ import KeyboardSettings from '@/components/keyboard-settings';
 export default function Header(): JSX.Element {
 	return (
 		<header className="header">
-			<h1>Digital Keyboard Tunes</h1>
+			<div className="header__main-items">
+				<h1>Digital Keyboard Tunes</h1>
 
-			<CustomButton ariaLabel="Open keyboard settings">
-				<Icon name="gear" />
-			</CustomButton>
+				<CustomButton ariaLabel="Open keyboard settings">
+					<Icon name="gear" />
+				</CustomButton>
+			</div>
 
 			<KeyboardSettings />
 		</header>
