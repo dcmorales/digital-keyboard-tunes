e6a3835813fc4878f9b8dd896ed90313246838ab
DC Morales <dc.morales37@gmail.com>
2024-09-28 17:44:21 -0700

    add first dropdown option

--- client/components/keyboard-settings.test.tsx
+++ client/components/keyboard-settings.test.tsx
@@ -6,9 +6,15 @@ import KeyboardSettings from './keyboard-settings';
 describe('Header', () => {
 	render(<KeyboardSettings />);
 
-	it('renders the keyboard settings', () => {
+	it('renders the keyboard settings group', () => {
 		expect(
 			screen.getByRole('group', { name: 'Keyboard settings' })
+		).toBeDefined();
+	});
+
+	it('renders the settings dropdowns', () => {
+		expect(
+			screen.getByRole('combobox', { name: 'Select a key' })
 		).toBeDefined();
 	});
 });
--- client/components/keyboard-settings.tsx
+++ client/components/keyboard-settings.tsx
@@ -1,9 +1,19 @@
 // keyboard-settings
 
+import { noteOptions } from '@/components/keyboard-full';
+
 export default function KeyboardSettings(): JSX.Element {
 	return (
 		<div role="group" aria-label="Keyboard settings">
-			keyboard-settings
+			<select aria-label="Select a key">
+				{noteOptions.map((note) => {
+					return (
+						<option key={`${note}`} value={`${note}`}>
+							{note}
+						</option>
+					);
+				})}
+			</select>
 		</div>
 	);
 }
