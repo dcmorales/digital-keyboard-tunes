4e9a8bd0efabc10cc39c23e70dfea204784218c0
DC Morales <dc.morales37@gmail.com>
2024-09-29 23:00:48 -0700

    update keyboard settings tests

--- client/components/keyboard-settings.test.tsx
+++ client/components/keyboard-settings.test.tsx
@@ -1,11 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import { beforeEach, describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 
+import { KeyboardOptionsProvider } from '@/context/keyboard-options-context';
 import KeyboardSettings from './keyboard-settings';
 
 describe('Keyboard settings', () => {
 	beforeEach(() => {
-		render(<KeyboardSettings />);
+		render(
+			<KeyboardOptionsProvider>
+				<KeyboardSettings />
+			</KeyboardOptionsProvider>
+		);
 	});
 
 	it('renders the keyboard settings group', () => {
@@ -14,9 +19,12 @@ describe('Keyboard settings', () => {
 		).toBeInTheDocument();
 	});
 
-	it('renders the settings dropdowns', () => {
-		expect(
-			screen.getByRole('combobox', { name: 'Select a key' })
-		).toBeInTheDocument();
+	it('renders the settings dropdowns and the options', () => {
+		const dropdown = screen.getByRole('combobox', { name: 'Select a key' });
+
+		expect(dropdown).toBeInTheDocument();
+		expect(dropdown).toHaveTextContent('C');
+		expect(dropdown).toHaveTextContent('D');
+		expect(dropdown).toHaveTextContent('E');
 	});
 });
--- client/components/keyboard-settings.tsx
+++ client/components/keyboard-settings.tsx
@@ -2,8 +2,11 @@
 
 import { noteOptions } from '@/components/keyboard-full';
 import Dropdown from '@/components/common/dropdown';
+import { useKeyboardOptions } from '@/context/keyboard-options-context';
 
 export default function KeyboardSettings(): JSX.Element {
+	const { selectedKey, onKeyChange } = useKeyboardOptions();
+
 	return (
 		<div role="group" aria-label="Keyboard settings">
 			<Dropdown
@@ -11,6 +14,8 @@ export default function KeyboardSettings(): JSX.Element {
 				ariaLabel="Select a key"
 				title="Key"
 				id="key"
+				value={selectedKey}
+				onChange={onKeyChange}
 			/>
 		</div>
 	);
