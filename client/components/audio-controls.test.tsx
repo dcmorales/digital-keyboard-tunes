48a5f50a9fb69ccf5f8417af128bf026cf1c13a7
DC Morales <dc.morales37@gmail.com>
2024-10-03 15:13:06 -0700

    create audio controls component

---
+++ client/components/audio-controls.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import AudioControls from './audio-controls';
+
+describe('Full Keyboard', () => {
+	beforeEach(() => {
+		render(<AudioControls />);
+	});
+
+	it('renders a labeled group div', () => {
+		const audioControls = screen.getByRole('group', {
+			name: 'Audio controls',
+		});
+
+		expect(audioControls).toBeInTheDocument();
+	});
+});
---
+++ client/components/audio-controls.tsx
@@ -0,0 +1,7 @@
+export default function AudioControls() {
+	return (
+		<div role="group" aria-label="Audio controls">
+			audio-controls
+		</div>
+	);
+}
