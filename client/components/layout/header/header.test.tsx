86a03dbc0142ed54e57c79fdddff71d83b3577d6
DC Morales <dc.morales37@gmail.com>
2024-10-04 14:09:52 -0700

    move header component into its own folder

--- client/components/layout/header/header.test.tsx
+++ client/components/layout/header/header.test.tsx
@@ -2,7 +2,7 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { beforeEach, describe, expect, it } from 'vitest';
 
 import { KeyboardOptionsProvider } from '@/context/keyboard-options-context';
-import Header from './header';
+import Header from '.';
 
 describe('Header', () => {
 	beforeEach(() => {
