ec5bd0737f6293e60d0b57595b0f025f324e3a9a
DC Morales <dc.morales37@gmail.com>
2024-10-05 16:30:52 -0700

    add comments for audio controls

--- client/components/audio-controls/index.tsx
+++ client/components/audio-controls/index.tsx
@@ -1,4 +1,8 @@
 // audio-controls
+// Contains the buttons that control whether the selected scale plays.
+// Clicking the play button will play each note in the selectedScaleNotes array and set
+// it as active (changing the appearance of the key). After a delay, it will then stop
+// the note and remove it from active (resetting the appearance of the key).
 
 import CustomButton from '@/components/common/custom-button';
 import Icon from '@/components/common/icon';
