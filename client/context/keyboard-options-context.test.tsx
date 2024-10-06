481fe7cff5ea65c59a97e8f5efe26578756cd16a
DC Morales <dc.morales37@gmail.com>
2024-10-05 19:25:33 -0700

    simplify test for defining scales

--- client/context/keyboard-options-context.test.tsx
+++ client/context/keyboard-options-context.test.tsx
@@ -180,52 +180,32 @@ describe('KeyboardOptionsProvider', () => {
 		expect(screen.getByText('Active Note: None')).toBeInTheDocument();
 	});
 
-	it('returns the correct set of selected notes based on octave and key', () => {
+	it('defines the correct set of selected notes based on octave and key', () => {
 		renderWithProvider();
 
 		changeSelection('Select Key:', 'D');
 		changeSelection('Select Octave:', '5');
 
-		expect(
-			screen.getByText(
-				'Selected Scale Notes: D5-E♭5-E5-F5-G♭5-G5-A♭5-A5-B♭5-B5-C6-D♭6-D6'
-			)
-		).toBeInTheDocument();
+		const scaleTests = [
+			{
+				scale: 'chromatic',
+				expected: 'D5-E♭5-E5-F5-G♭5-G5-A♭5-A5-B♭5-B5-C6-D♭6-D6',
+			},
+			{ scale: 'major', expected: 'D5-E5-G♭5-G5-A5-B5-D♭6-D6' },
+			{ scale: 'natural minor', expected: 'D5-E5-F5-G5-A5-B♭5-C6-D6' },
+			{ scale: 'harmonic minor', expected: 'D5-E5-F5-G5-A5-B♭5-D♭6-D6' },
+			{ scale: 'melodic minor', expected: 'D5-E5-F5-G5-A5-B5-D♭6-D6' },
+			{ scale: 'major pentatonic', expected: 'D5-E5-G♭5-A5-B5-D6' },
+			{ scale: 'minor pentatonic', expected: 'D5-F5-G5-A5-C6-D6' },
+			{ scale: 'blues', expected: 'D5-F5-G5-A♭5-A5-C6-D6' },
+		];
 
-		changeSelection('Select Scale:', 'major');
-		expect(
-			screen.getByText('Selected Scale Notes: D5-E5-G♭5-G5-A5-B5-D♭6-D6')
-		).toBeInTheDocument();
-
-		changeSelection('Select Scale:', 'natural minor');
-		expect(
-			screen.getByText('Selected Scale Notes: D5-E5-F5-G5-A5-B♭5-C6-D6')
-		).toBeInTheDocument();
-
-		changeSelection('Select Scale:', 'harmonic minor');
-		expect(
-			screen.getByText('Selected Scale Notes: D5-E5-F5-G5-A5-B♭5-D♭6-D6')
-		).toBeInTheDocument();
-
-		changeSelection('Select Scale:', 'melodic minor');
-		expect(
-			screen.getByText('Selected Scale Notes: D5-E5-F5-G5-A5-B5-D♭6-D6')
-		).toBeInTheDocument();
-
-		changeSelection('Select Scale:', 'major pentatonic');
-		expect(
-			screen.getByText('Selected Scale Notes: D5-E5-G♭5-A5-B5-D6')
-		).toBeInTheDocument();
-
-		changeSelection('Select Scale:', 'minor pentatonic');
-		expect(
-			screen.getByText('Selected Scale Notes: D5-F5-G5-A5-C6-D6')
-		).toBeInTheDocument();
-
-		changeSelection('Select Scale:', 'blues');
-		expect(
-			screen.getByText('Selected Scale Notes: D5-F5-G5-A♭5-A5-C6-D6')
-		).toBeInTheDocument();
+		scaleTests.forEach(({ scale, expected }) => {
+			changeSelection('Select Scale:', scale);
+			expect(
+				screen.getByText(`Selected Scale Notes: ${expected}`)
+			).toBeInTheDocument();
+		});
 	});
 
 	it('throws an error when used outside of the provider', () => {
