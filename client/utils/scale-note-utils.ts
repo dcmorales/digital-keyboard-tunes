2b6d6bfa0d2ea7a321aedd275eea4e2cfaba3b86
DC Morales <dc.morales37@gmail.com>
2024-10-07 19:04:00 -0700

    simplify switch case for defining scales

--- client/utils/scale-note-utils.ts
+++ client/utils/scale-note-utils.ts
@@ -43,29 +43,31 @@ export function defineScaleNotes(
 	switch (selectedScale) {
 		case 'major':
 			scaleNoteIndexes = [0, 2, 4, 5, 7, 9, 11, 12];
-			return scaleNoteIndexes.map((index) => fullNotesOctave[index]);
+			break;
 		case 'natural minor':
 			scaleNoteIndexes = [0, 2, 3, 5, 7, 8, 10, 12];
-			return scaleNoteIndexes.map((index) => fullNotesOctave[index]);
+			break;
 		case 'harmonic minor':
 			scaleNoteIndexes = [0, 2, 3, 5, 7, 8, 11, 12];
-			return scaleNoteIndexes.map((index) => fullNotesOctave[index]);
+			break;
 		case 'melodic minor':
 			scaleNoteIndexes = [0, 2, 3, 5, 7, 9, 11, 12];
-			return scaleNoteIndexes.map((index) => fullNotesOctave[index]);
+			break;
 		case 'major pentatonic':
 			scaleNoteIndexes = [0, 2, 4, 7, 9, 12];
-			return scaleNoteIndexes.map((index) => fullNotesOctave[index]);
+			break;
 		case 'minor pentatonic':
 			scaleNoteIndexes = [0, 3, 5, 7, 10, 12];
-			return scaleNoteIndexes.map((index) => fullNotesOctave[index]);
+			break;
 		case 'blues':
 			scaleNoteIndexes = [0, 3, 5, 6, 7, 10, 12];
-			return scaleNoteIndexes.map((index) => fullNotesOctave[index]);
+			break;
 		default:
 			// chromatic
-			return fullNotesOctave;
+			scaleNoteIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 	}
+
+	return scaleNoteIndexes.map((index) => fullNotesOctave[index]);
 }
 
 // Fisher-Yates shuffle
