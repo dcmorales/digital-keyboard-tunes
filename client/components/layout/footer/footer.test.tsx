a23675ed5c0b28f9ef1206853a023f8592351cda
DC Morales <dc.morales37@gmail.com>
2024-10-16 00:08:08 -0700

    add repo link to footer

--- client/components/layout/footer/footer.test.tsx
+++ client/components/layout/footer/footer.test.tsx
@@ -13,4 +13,16 @@ describe('Footer', () => {
 
 		expect(footer).toBeInTheDocument();
 	});
+
+	it('contains a link to the GitHub repository', () => {
+		const link = screen.getByRole('link', { name: /Github Repository/i });
+
+		expect(link).toBeInTheDocument();
+		expect(link).toHaveAttribute('target', '_blank');
+		expect(link).toHaveAttribute('rel', 'noopener noreferrer');
+		expect(link).toHaveAttribute(
+			'href',
+			'https://github.com/dcmorales/digital-keyboard-tunes'
+		);
+	});
 });
--- client/components/layout/footer/index.tsx
+++ client/components/layout/footer/index.tsx
@@ -1,7 +1,19 @@
 // footer
 
+import Icon from '@/components/common/icon';
 import styles from './footer.module.scss';
 
 export default function Footer() {
-	return <footer className={styles.footer}>Footer</footer>;
+	return (
+		<footer className={styles.footer}>
+			<a
+				className={styles.link}
+				href="https://github.com/dcmorales/digital-keyboard-tunes"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<Icon name="github" /> Github Repository
+			</a>
+		</footer>
+	);
 }
