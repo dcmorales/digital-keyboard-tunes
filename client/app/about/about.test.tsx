774e02781996305fe0fd097a6ca43e818f90b461
DC Morales <dc.morales37@gmail.com>
2024-10-17 12:51:33 -0700

    add sections to about page

--- client/app/about/about.test.tsx
+++ client/app/about/about.test.tsx
@@ -3,27 +3,32 @@ import { beforeEach, describe, expect, it } from 'vitest';
 
 import AboutPage from './page';
 
-describe('Notes Display', () => {
+describe('About Page', () => {
 	beforeEach(() => {
 		render(<AboutPage />);
 	});
 
-	it('renders the headings', () => {
+	it('renders the sections', () => {
 		const firstHeading = screen.getByRole('heading', {
 			level: 2,
 			name: /What is it/i,
 		});
+		const firstSection = firstHeading.closest('section');
+
 		const secondHeading = screen.getByRole('heading', {
 			level: 2,
 			name: /Settings/i,
 		});
+		const secondSection = secondHeading.closest('section');
+
 		const thirdHeading = screen.getByRole('heading', {
 			level: 2,
 			name: /Audio controls/i,
 		});
+		const thirdSection = thirdHeading.closest('section');
 
-		expect(firstHeading).toBeInTheDocument();
-		expect(secondHeading).toBeInTheDocument();
-		expect(thirdHeading).toBeInTheDocument();
+		expect(firstSection).toBeInTheDocument();
+		expect(secondSection).toBeInTheDocument();
+		expect(thirdSection).toBeInTheDocument();
 	});
 });
--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -3,9 +3,30 @@ import styles from './about.module.scss';
 export default function AboutPage() {
 	return (
 		<article className={styles.about}>
-			<h2>What is it</h2>
-			<h2>Settings</h2>
-			<h2>Audio controls</h2>
+			<section>
+				<h2>What is it</h2>
+			</section>
+
+			<section>
+				<h2>Settings</h2>
+				<h3>Key</h3>
+				<h3>Octave</h3>
+				<h3>Waveform</h3>
+				<h3>Scale</h3>
+				<h3>Order</h3>
+				<h3>Note Length</h3>
+				<h3>BPM</h3>
+				<h3>Total Notes</h3>
+				<h3>Repeat</h3>
+			</section>
+
+			<section>
+				<h2>Audio controls</h2>
+				<h3>Play</h3>
+				<h3>Shuffle</h3>
+				<h3>Repeat</h3>
+				<h3>Stop</h3>
+			</section>
 		</article>
 	);
 }
