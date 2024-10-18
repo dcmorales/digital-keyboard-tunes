2d067957bffe6f35e9e5fe094bf7a994659f899d
DC Morales <dc.morales37@gmail.com>
2024-10-18 13:12:47 -0700

    create tooltip component

---
+++ client/components/common/tooltip/index.tsx
@@ -0,0 +1,55 @@
+// tooltip
+
+'use client';
+
+import { useState } from 'react';
+
+import Icon from '@/components/common/icon';
+import styles from './tooltip.module.scss';
+
+interface TooltipProps {
+	topic: string;
+	text: string;
+}
+
+export default function Tooltip({ topic, text }: TooltipProps) {
+	const [isVisible, setIsVisible] = useState(false);
+
+	const handleTooltipToggle = (): void => {
+		setIsVisible((prev) => !prev);
+	};
+
+	const showTooltip = (): void => {
+		setIsVisible(true);
+	};
+
+	const hideTooltip = (): void => {
+		setIsVisible(false);
+	};
+
+	return (
+		<div className={styles.tooltipContainer} aria-describedby="tooltip">
+			<button
+				aria-label={`Information for ${topic}`}
+				onClick={handleTooltipToggle}
+				onMouseEnter={showTooltip}
+				onMouseLeave={hideTooltip}
+				onFocus={showTooltip}
+				onBlur={hideTooltip}
+			>
+				<Icon name="info" size="x-small" />
+			</button>
+
+			{isVisible && (
+				<div
+					id="tooltip"
+					role="tooltip"
+					className={styles.tooltip}
+					aria-hidden={!isVisible}
+				>
+					{text}
+				</div>
+			)}
+		</div>
+	);
+}
